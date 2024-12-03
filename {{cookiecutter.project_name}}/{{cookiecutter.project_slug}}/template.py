5a1b9f69afa0906b3033742b698a435f1ea7fb45
Lee Skillen <lskillen@cloudsmith.io>
2024-12-03 12:00:44 +0000

    fix: template

--- {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/template.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/template.py
@@ -5,16 +5,26 @@ from . import utils
 
 
 def example() -> str:
+    """Get example input."""
     return """
 EXAMPLE
 """
 
 
-def read_data(data: list[str] | None = None) -> tuple[list[int], list[int]]:
-    data = data or utils.read_example(example())
+def read_data(data: list[str] | None = None) -> list[str]:
+    """Read the something."""
+    return data or utils.read_example(example())
 
 
 def solve(data: list[str] | None = None) -> tuple[int, int]:
+    """
+    Solve the something.
+
+    You can write docstests here too:
+
+    >>> solve()
+    (0, 0)
+    """
     data = read_data(data=data)  # noqa: F841 (delete this)
     p1, p2 = 0, 0
     return p1, p2
